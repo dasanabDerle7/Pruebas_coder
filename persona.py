5c3cf02d220788472342947848845936aa157091
dasanabria7.as <dasanabria7.as@gmail.com>
2024-09-16 23:37:00 -0300

    se agrega atributo a persona

--- persona.py
+++ persona.py
@@ -1,8 +1,16 @@
 class peronsa():
-    def __init__(self, nombre, apellido):
+    def __init__(self, nombre, apellido,edad):
         self.nombre=nombre
         self.apellido=apellido
+        self.edad=edad
         
     def saluda(sefl):
        return f'Hola soy {sefl.nombre}'
-   # Cuando modifico debo agregar al stagin area para poder comitear
+   # Cuando modifico debo agregar al stagin area para poder commit
+   
+   ## Con git checkout --oneline . volvemos al ultimo estado
+   
+   ## git branch vemos todos las ramas que tenemos, si le agregamos el nombre, generamos una rama
+    ## Con git checkout volvemos a la rama que querramos con el nombre
+    
+    
