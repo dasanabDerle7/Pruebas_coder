875fb6b8841a7793104e97ddc70073e37dfab090
dasanabria7.as <dasanabria7.as@gmail.com>
2024-09-17 00:10:17 -0300

    Agregi una funcion

--- animales.py
+++ animales.py
@@ -4,3 +4,7 @@ class perro():
         
         
 ## Para cruzar los datos debemos podemos hacer el merge
+## Para eliminar una rama hago git branch -D "nombre de rama"
+
+    def ladrar(self):
+        return f'Guau guau'
